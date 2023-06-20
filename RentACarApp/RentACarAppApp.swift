e3fcc09a19a8ecee0d6d0e31a038f960844b40dc
Özenç Yeni <ozencyeni@gmail.com>
2023-06-20 14:16:01 +0300

    Initial Commit

---
+++ RentACarApp/RentACarAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  RentACarAppApp.swift
+//  RentACarApp
+//
+//  Created by Özenç Yeni on 20.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct RentACarAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
